836dc62f3f22ce114cab3e05c842b651f4ce216a
CristianRuval <altcris19@gmail.com>
2025-10-31 17:29:05 -0500

    Update sessionTest.py
    corrected some test case wording

--- sessionTest.py
+++ sessionTest.py
@@ -4,15 +4,16 @@ userInstance = UserSession()
 class TestBase(unittest.TestCase):
     def test_Base(self):
         testCase = userInstance.endUserSession("Cris", True, True)
-        self.assertEqual(testCase, True, 'correctly logs user session')
+        self.assertEqual(testCase, True, 'base case, all correct inputs')
 class testExpired(unittest.TestCase):
     def test_Expired(self):
         testCase = userInstance.endUserSession("Cris", False, True)
-        self.assertEqual(testCase, False, 'correctly logs user session')
+        self.assertEqual(testCase, False, 'correctly informs user to log in as there is no valid session')
 
 class testNetwork(unittest.TestCase):
     def test_Network(self):
         testCase = userInstance.endUserSession("Cris", True, False)
         self.assertEqual(testCase, False, 'correctly ends session as there is no connection')
 if __name__ == '__main__':
+
     unittest.main()
