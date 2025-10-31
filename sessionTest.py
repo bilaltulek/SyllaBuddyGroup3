2ad73951494318f455004c77201ffc1d7c46b769
CristianRuval <altcris19@gmail.com>
2025-10-31 18:55:36 -0500

    Update sessionTest.py
    added null testing

--- sessionTest.py
+++ sessionTest.py
@@ -14,6 +14,19 @@ class testNetwork(unittest.TestCase):
     def test_Network(self):
         testCase = userInstance.endUserSession("Cris", True, False)
         self.assertEqual(testCase, False, 'correctly ends session as there is no connection')
+class nullName(unittest.TestCase):
+    def test_nName(self):
+        testCase = userInstance.endUserSession("NULL", True, False)
+        self.assertEqual(testCase, False, 'correctly ends instance due to null exception')
+class nullLogin(unittest.TestCase):
+    def test_nLogin(self):
+        testCase = userInstance.endUserSession("Cris", "NULL", False)
+        self.assertEqual(testCase, False, 'correctly ends instance due to null exception')
+class nullNetwork(unittest.TestCase):
+    def test_nNetwork(self):
+        testCase = userInstance.endUserSession("cris", True, "NULL")
+        self.assertEqual(testCase, False, 'correctly ends instance due to null exception')
 if __name__ == '__main__':
 
     unittest.main()
+
